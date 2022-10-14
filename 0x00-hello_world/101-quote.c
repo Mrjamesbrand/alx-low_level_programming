ab4fc24a0b58b90855c846428dc0691d2ddf86cc
mrjamsbrand <askjamesfranklin@gmail.com>
2022-10-13 18:07:16 -0700

    101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include < studio.h>
+#include < unistd.h>
+/**
+ * main - Entry
+ * Return:
+ */
+int main (void)
+{
+	write (1, "and that piece of art is useful\"  -Dora Korpar, 2015-101-19\n",59);
+	return (1)
+	
